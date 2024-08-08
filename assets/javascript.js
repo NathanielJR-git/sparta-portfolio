491e62db9a0bff925b5d49707003100866f317d1
Nathaniel Jonathan Rusli <omgitsnathaniels@gmail.com>
2024-08-09 02:30:53 +0700

    feat: add laser eye functionality

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM fully loaded and parsed");
+  
+  const imageElement2 = document.getElementById("image-to-replace-2")
+
+  const imageElement = document.getElementById("image-to-replace");
+  if (!imageElement) {
+    console.error("Element not found");
+    return;
+  }
+
+  const newImageSrc = "assets/images/self-photo-burnt.png"; // Image to replace with
+
+  imageElement2.addEventListener("mouseover", function() {
+    // Change the image source on hover
+    imageElement.src = newImageSrc;
+
+    // Optional: Remove event listener after first hover
+    imageElement.removeEventListener("mouseover", arguments.callee);
+  });
+});
+
+
 function scrollToCoordinate() {
   window.scrollTo({
     top: 0,
